b3ed82ed09b09341307974899743b0afc4564aec
Jacob Mee <jacob@mitang.me>
2016-06-03 00:27:30 +0800

    Added dnsmasq for Apple

--- update.sh
+++ update.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-python /etc/gfwlist2dnsmasq/gfwlist2dnsmasq.py
-echo "Copy file to www"
-cp /etc/gfwlist2dnsmasq/dnsmasq_list.conf /var/www
+
+# Generating dnsmasq file for blocked sites
+cd /etc/dnsupdate/gfwlist2dnsmasq/
+python /etc/dnsupdate/gfwlist2dnsmasq/gfwlist2dnsmasq.py
+echo "Copy sites file to www"
+mv /etc/dnsupdate/gfwlist2dnsmasq/dnsmasq_list.conf /var/www
+
+# Generating DNS for apple ChinaNet
+cd /etc/dnsupdate/appleDNS
+python fetch-timeout.py ChinaNet.json
+python export-configure.py merlin > dnsmasq_apple.conf
+echo "Copy APPLE hosts to www"
+mv /etc/dnsupdate/appleDNS/dnsmasq_apple.conf /var/www
